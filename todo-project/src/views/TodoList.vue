c9f3d96a440e0138951e3ab04b46467f2abb80cb
vue.js2 <camilapalacios112@gmail.com>
2024-08-09 10:25:06 -0400

    cambios realizados

--- todo-project/src/views/TodoList.vue
+++ todo-project/src/views/TodoList.vue
@@ -6,28 +6,17 @@
       <el-input placeholder="todo" v-model="todo"></el-input>
     </form>
     <el-row :gutter="12">
-      <!-- área de visualización de tareas pendientes -->
       <el-col :span="12" v-for="( todo, index ) in todos" :key="index">
-        <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
-          <el-row :gutter="12">
-            <el-col :span="21">{{ todo }}</el-col>
-            <el-col :span="3">
-              <el-button @click="removeTodo(index)" type="success" icon="el-icon-check" circle></el-button>
+                <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
+                    <el-row :gutter="12">
+                        <el-col :span="21">{{ todo }}</el-col>
+                        <el-col :span="3">
+                            <el-button @click="removeTodo(index)" type="success" icon="el-icon-check"
+                                circle></el-button>
+                        </el-col>
+                    </el-row>
+                </el-card>
             </el-col>
-          </el-row>
-        </el-card>
-      </el-col>
-      <!-- zona de visualización de problemas -->
-      <el-col :span="12" v-for="( issue, index ) in issues" :key="issue.id">
-        <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
-          <el-row :gutter="12">
-            <el-col :span="21">{{ issue.title }}</el-col>
-            <el-col :span="3">
-              <el-button @click="closeIssue(index)" type="success" icon="el-icon-check" circle></el-button>
-            </el-col>
-          </el-row>
-        </el-card>
-      </el-col>
     </el-row>
   </div>
 </template>
